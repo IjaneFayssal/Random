084f2254bcfe5059410fa96ada8ed2b0cc515fea
IjaneFayssal <76232773+IjaneFayssal@users.noreply.github.com>
2020-12-19 10:45:28 +0100

    Update asdasd.js

--- asdasd.js
+++ asdasd.js
@@ -1,3 +1,14 @@
+export function input(msg = "") {
+    const buf = new Uint8Array(1024);
+    Deno.stdout.write(new TextEncoder().encode(msg));
+    const n = Deno.stdin.read(buf);
+    return new TextDecoder().decode(buf.subarray(0, n)).trim();
+}
+
+export function print(msg = "") {
+    console.log(msg);
+}
+
 export function randomInt(min = 0, max = 1) {
   min = Math.ceil(min);
   max = Math.floor(max);
